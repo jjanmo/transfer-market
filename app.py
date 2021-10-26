7d1d9b1f83319ca0f2c737e07727e152fda1354a
jjanmo <jjanmo07@hanmail.net>
2021-10-26 22:18:40 +0900

    feat: add flask

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+from main import crawler
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World 🔥"
+    data = crawler()
+    print(data)
+    return render_template('index.html', user='jjanmo', data={'a': 10, 'b': 100})
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -19,34 +19,36 @@ player_info = {
     'market_value': [],
 }
 
-for page in range(4):
-    url = f'https://www.transfermarkt.com/spieler-statistik/wertvollstespieler/marktwertetop?ajax=yw1&page={page + 1}'
+def get_data():
+    for page in range(4):
+        url = f'https://www.transfermarkt.com/spieler-statistik/wertvollstespieler/marktwertetop?ajax=yw1&page={page + 1}'
 
-    r = requests.get(url, headers=headers)
-    print(r.status_code)
+        r = requests.get(url, headers=headers)
+        print(r.status_code)
 
-    soup = BeautifulSoup(r.text, 'html.parser')  # r.text -> r.content
+        soup = BeautifulSoup(r.text, 'html.parser')  # r.text -> r.content
 
-    player_list = soup.find_all('tr', {'class': ['odd', 'even']})
+        player_list = soup.find_all('tr', {'class': ['odd', 'even']})
 
-    for player in player_list:
-        columns = player.find_all('td')
+        for player in player_list:
+            columns = player.find_all('td')
 
-        player_info['ranking'].append(columns[0].text)
-        player_info['name'].append(columns[3].text)
-        player_info['profile'].append(columns[1].img['src'])
-        player_info['position'].append(columns[4].text)
-        player_info['age'].append(columns[5].text)
-        player_info['nationality'].append(columns[6].img['title'])
-        player_info['flag'].append(columns[6].img['src'])
-        player_info['club'].append(columns[7].img['alt'])
-        player_info['club_logo'].append(columns[7].img['src'])
-        player_info['market_value'].append(columns[8].b.text)
+            player_info['ranking'].append(columns[0].text)
+            player_info['name'].append(columns[3].text)
+            player_info['profile'].append(columns[1].img['src'])
+            player_info['position'].append(columns[4].text)
+            player_info['age'].append(columns[5].text)
+            player_info['nationality'].append(columns[6].img['title'])
+            player_info['flag'].append(columns[6].img['src'])
+            player_info['club'].append(columns[7].img['alt'])
+            player_info['club_logo'].append(columns[7].img['src'])
+            player_info['market_value'].append(columns[8].b.text)
 
-        time.sleep(1)
+            time.sleep(1)
 
+    return player_info
 
-df = pd.DataFrame(player_info)
+    # df = pd.DataFrame(player_info)
 
-# export csv file
-df.to_csv('data/transfermarket100.csv', index=False)
+    # export csv file
+    # df.to_csv('data/transfermarket100.csv', index=False)
